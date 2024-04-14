4a3400d62168605d052efe1615c9b30dd70ce7d5
Miguel Ceballos <mceballosleonides@gmail.com>
2024-04-13 22:42:40 -0600

    Modied components

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,5 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import AppLayout from "@/Layouts/AppLayout.vue";
 </script>
 
 <template>
@@ -10,11 +9,10 @@ import Welcome from '@/Components/Welcome.vue';
                 Dashboard
             </h2>
         </template>
-
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                <div class="bg-white overflow-hidden sm:rounded-lg">
+                    <h1>hello world</h1>
                 </div>
             </div>
         </div>

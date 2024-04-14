da92de3d08a0189c11b2f7ed4d7b3fd8ca93d1e6
Miguel Ceballos <mceballosleonides@gmail.com>
2024-04-13 23:41:34 -0600

    Rename Categories/Show to Categories/Edit

---
+++ resources/js/Pages/Categories/Edit.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import FormCategories from "@/Components/Categories/FormCategories.vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+</script>
+
+<template>
+    <AppLayout title="Categories">
+        <template #header>
+            <h2 class="font-bold text-3xl text-gray-800 leading-tight">
+                Create a new category
+            </h2>
+        </template>
+
+        <div class="py-10">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden">
+                    <FormCategories :form="form" @submit="form.post(route('categories.store'))" :updating="true"/>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>

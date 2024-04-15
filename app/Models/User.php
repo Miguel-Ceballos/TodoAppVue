9c337eb168333ca4e504238973f7bac0f00ba738
Miguel Ceballos <mceballosleonides@gmail.com>
2024-04-14 23:05:57 -0600

    Created relations between users and statuses tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,4 +68,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Category::class);
     }
+
+    public function statuses(): HasMany
+    {
+        return $this->hasMany(Status::class);
+    }
 }

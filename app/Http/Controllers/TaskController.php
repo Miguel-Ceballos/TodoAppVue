0f181ab956eb31aa4694f4e86d20dd307f9a485c
Miguel Ceballos <mceballosleonides@gmail.com>
2024-04-15 13:55:11 -0600

    Created migration, controller, factory, request and model to tasks table

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TaskRequest;
+use App\Models\Category;
+use App\Models\Status;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Category $category)
+    {
+        return inertia('Tasks/Index', [ 'tasks' => $category->tasks, 'category' => $category ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Category $category)
+    {
+        return inertia('Tasks/Create', [ 'statuses' => Status::all(), 'category' => $category ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TaskRequest $request, Category $category)
+    {
+
+//        dd($request);
+        Task::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'category_id' => $category->id,
+            'status_id' => $request->status_id,
+            'user_id' => auth()->user()->id
+        ]);
+        return redirect()->route('tasks.index', $category->id);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category, Task $task)
+    {
+        return inertia('Tasks/Edit', [ 'task' => $task, 'statuses' => Status::all(), 'category' => $category ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Category $category, TaskRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+        return redirect()->route('tasks.index', $category->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect()->route('tasks.index');
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function status(): HasOne
+    {
+        return $this->hasOne(Status::class, 'status_id');
+    }
+}

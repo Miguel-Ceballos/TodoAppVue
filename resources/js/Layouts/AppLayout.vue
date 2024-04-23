363fa7fc745fb317be8b66fbdf0de197703cc747
Miguel Ceballos <mceballosleonides@gmail.com>
2024-04-22 19:44:51 -0600

    Modified svg width

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -97,7 +97,7 @@ const logout = () => {
                 <li v-for="category in $page.props.user.categories">
                     <NavLink :href="route('tasks.index', category.slug)"
                              :active="route().current('tasks.*', category.slug)">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
                         </svg>
                         <span class="ms-3">{{ category.name }}</span>

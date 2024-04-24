41bf5570fbd48b5411ab1315d173c9908841a321
Miguel-Ceballos <mceballosleonides@gmail.com>
2024-04-23 22:47:51 -0600

    Added use Pinia to App.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,10 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import 'flowbite';
+import {createPinia} from "pinia";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const pinia = createPinia()
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -16,6 +18,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(pinia)
             .mount(el);
     },
     progress: {

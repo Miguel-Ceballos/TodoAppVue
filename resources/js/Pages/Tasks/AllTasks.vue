55d86a6f81a430f24247a3018771908fc2afe9d2
Miguel-Ceballos <mceballosleonides@gmail.com>
2024-04-24 14:37:34 -0600

    Add deleteTask to Props Modal and changed ul tasks

--- resources/js/Pages/Tasks/AllTasks.vue
+++ resources/js/Pages/Tasks/AllTasks.vue
@@ -49,7 +49,6 @@ defineProps({
                 <span class="text-gray-800 hover:text-indigo-600">Add Task</span>
             </button>
 
-            <!-- Crete tasks modal -->
             <CustomModal :modal="modal.modal"
                          :form="store.form"
                          :categories="categories"
@@ -57,43 +56,20 @@ defineProps({
                          :update="modal.isUpdate"
                          @submit="store.action"
                          @handleClickModal="modal.handleClickModal(store.form)"
+                         @deleteTask="store.deleteTask(modal.currentItem)"
             />
 
             <ul v-if="tasks.length > 0" role="list">
                 <li v-for="task in tasks"
-                    class="flex justify-between items-center gap-2 md:gap-x-6 py-5 p-2 border-b"
+                    class="flex items-center gap-2 md:gap-x-6 py-5 p-2 border-b hover:cursor-pointer hover:bg-gray-50"
                 >
-                    <div class="flex gap-4 text-gray-600">
-                        <Checkbox/>
+                    <Checkbox/>
+                    <div class="flex  w-full gap-4 text-gray-600" @click="modal.handleClickModalUpdate(task, store.form)">
                         <p class="text-sm text-gray-800">{{ task.title }}</p>
-                    </div>
-                    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                        <div class="flex justify-center gap-2">
-                            <button
-                                @click="modal.handleClickModalUpdate(task, store.form)"
-                                class="text-md leading-6 text-gray-600 hover:bg-slate-100 p-1 rounded-md"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"/>
-                                </svg>
-                            </button>
-                            <button
-                                @click="store.deleteTask(task.id)"
-                                class="text-md leading-6 text-gray-600 hover:bg-slate-100 p-1 rounded-md"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
-                                </svg>
-                            </button>
-                        </div>
                     </div>
                 </li>
             </ul>
-            <p v-else class="mt-4 text-center">No hay notas existentes</p>
+            <p v-else class="mt-4">There are no tasks.</p>
         </div>
     </AppLayout>
 </template>

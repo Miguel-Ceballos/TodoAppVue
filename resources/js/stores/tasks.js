dc2d221160970ee0b895a8f4ad99bcc4ce68e68c
Miguel-Ceballos <mceballosleonides@gmail.com>
2024-04-23 23:19:42 -0600

    Created tasks store

---
+++ resources/js/stores/tasks.js
@@ -0,0 +1,55 @@
+import {defineStore} from "pinia";
+import {router, useForm} from "@inertiajs/vue3";
+import {useModalStore} from "@/stores/modal.js";
+
+export const useTasksStore = defineStore('tasks', () => {
+
+    const modal = useModalStore()
+
+    const form = useForm({
+        title: '',
+        description: '',
+        status_id: {
+            type: Number
+        },
+        category_id: {
+            type: Number
+        }
+    })
+
+    function storeTask() {
+        form.post(route('inbox.store'))
+        router.on('success', (event) => {
+            modal.modal = false
+            form.reset()
+        })
+    }
+
+    function updateTask() {
+        form.put(route('inbox.update', [modal.currentItem]))
+        router.on('success', (event) => {
+            modal.modal = false
+            form.reset()
+        })
+    }
+
+    function deleteTask(id) {
+        if (confirm('Are you sure?')) {
+            router.delete(route('inbox.destroy', [id]), {preserveScroll: true})
+        }
+    }
+
+    function action() {
+        if (!modal.isUpdate) {
+            storeTask()
+            return
+        }
+        updateTask()
+    }
+
+    return {
+        form,
+        action,
+        deleteTask
+    }
+})

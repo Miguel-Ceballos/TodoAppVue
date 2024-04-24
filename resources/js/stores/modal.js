f550a40d20a295e700af8cfa7161afa9c32f9d24
Miguel-Ceballos <mceballosleonides@gmail.com>
2024-04-23 23:20:53 -0600

    Deleted unused functions

--- resources/js/stores/modal.js
+++ resources/js/stores/modal.js
@@ -1,74 +1,29 @@
 import {defineStore} from "pinia";
 import {ref} from "vue";
-import {router, useForm} from "@inertiajs/vue3";
-
 
 export const useModalStore = defineStore('modal', () => {
 
     const modal = ref(false)
     const isUpdate = ref(false)
-    const currentTask = ref(Number)
+    const currentItem = ref(Number)
 
-    const form = useForm({
-        title: '',
-        description: '',
-        status_id: {
-            type: Number
-        },
-        category_id: {
-            type: Number
-        }
-    })
-
-    function storeTask() {
-        form.post(route('inbox.store'))
-        router.on('success', (event) => {
-            modal.value = false
-            form.reset()
-        })
-    }
-
-    function updateTask() {
-        form.put(route('inbox.update', [currentTask.value]))
-        router.on('success', (event) => {
-            modal.value = false
-            form.reset()
-        })
-    }
-
-    function deleteTask(id) {
-        if (confirm('Are you sure?')) {
-            router.delete(route('inbox.destroy', [id]), {preserveScroll: true})
-        }
-    }
-
-    function handleClickModal() {
+    function handleClickModal(form) {
         isUpdate.value = false
         modal.value = !modal.value
         form.reset()
     }
 
-    function handleClickModalUpdate(task) {
-        currentTask.value = task.id
+    function handleClickModalUpdate(task, form) {
+        currentItem.value = task.id
         isUpdate.value = true
         Object.assign(form, task)
         modal.value = !modal.value
     }
 
-    function action() {
-        if (!isUpdate.value) {
-            storeTask()
-            return
-        }
-        updateTask()
-    }
-
     return {
-        form,
         modal,
         isUpdate,
-        action,
-        deleteTask,
+        currentItem,
         handleClickModal,
         handleClickModalUpdate
     }

857d2393e218b70f51e47908f6f389b1c9bb26a5
Miguel Ceballos <mceballosleonides@gmail.com>
2024-04-30 11:57:53 -0600

    Added Task from sidebar

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -5,6 +5,12 @@ import Banner from '@/Components/Banner.vue';
 import NavLink from "@/Components/NavLink.vue";
 import { initFlowbite } from 'flowbite'
 import {onMounted} from "vue";
+import {useModalStore} from "@/stores/modal.js";
+import {useAllTasksStore} from "@/stores/all-tasks.js";
+import AllTasksForm from "@/Components/AllTasks/AllTasksForm.vue";
+
+const modal = useModalStore()
+const store = useAllTasksStore()
 
 // initialize components based on data attribute selectors
 onMounted(() => {
@@ -57,6 +63,28 @@ const logout = () => {
             <h1 class="text-2xl text-center my-4 text-slate-700">My <span class="font-bold">TODO</span> list</h1>
             <ul class="space-y-1 font-medium">
                 <li>
+                    <button @click="modal.handleClickModal(store.form)"
+                            class="flex items-center pl-0 text-sm font-light text-indigo-600 rounded-sm hover:bg-slate-100 group transition duration-150 ease-in-out w-full gap-2"
+                            type="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                             class="w-8 h-8">
+                            <path fill-rule="evenodd"
+                                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                        <span class="font-bold">Add Task</span>
+                    </button>
+
+                    <AllTasksForm :modal="modal.modal"
+                                  :form="store.form"
+                                  :categories="$page.props.user.categories"
+                                  :update="modal.isUpdate"
+                                  @submit="store.action"
+                                  @handleClickModal="modal.handleClickModal(store.form)"
+                                  @deleteTask="store.deleteTask(modal.currentItem)"
+                    />
+                </li>
+                <li>
                     <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
@@ -67,23 +95,33 @@ const logout = () => {
                 <li>
                     <NavLink :href="route('inbox.index')" :active="route().current('inbox.index')">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m6.75 12H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                         </svg>
                         <span class="ms-3">Tasks</span>
                     </NavLink>
                 </li>
+<!--                <li>-->
+<!--                    <NavLink :href="route('inbox.index')" :active="route().current('inbox.index')">-->
+<!--                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">-->
+<!--                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />-->
+<!--                        </svg>-->
+<!--                        <span class="ms-3">Today</span>-->
+<!--                    </NavLink>-->
+<!--                </li>-->
                 <li>
-                    <NavLink :href="route('categories.index')" :active="route().current('categories.*')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"/>
+                    <NavLink>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap">Categories</span>
+                        <span class="ms-3">Completed</span>
                     </NavLink>
                 </li>
                 <br>
-                <li>Categories</li>
+                <li>
+                    <NavLink :href="route('categories.index')" :active="route().current('categories.*')" class="flex items-center ms-0">
+                        <span class="whitespace-nowrap text- w-6 h-6 p-1 font-bold">Categories</span>
+                    </NavLink>
+                </li>
                 <li v-if="$page.props.user.categories.length > 0" v-for="category in $page.props.user.categories">
                     <NavLink :href="route('tasks.index', category.slug)"
                              :active="route().current('tasks.*', category.slug)">
